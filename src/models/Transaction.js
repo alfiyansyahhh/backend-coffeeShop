dfd158de73396196d5f6b3db7d7cf12de5870bbc
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-11-21 10:17:08 +0700

    tr

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -18,17 +18,14 @@ const transactionModel = {
       },
     );
   }),
-  getList: (search, field, typeSort, limit, offset) => new Promise((resolve, reject) => {
+  getList: () => new Promise((resolve, reject) => {
     db.query(
       `select dt.id,id_user,u.display_name,alamat,payment,shipping,subtotal,tax,total,p.id_product,p.price,product_name,category,p.categoryID 
         from details_transaction as dt
         left join transaction as t on dt.id_masterTransaction=t.id 
         left join users as u on t.id_user=u.id 
         left join product as p on dt.id_product=p.id_product 
-        left join category as c on p.categoryID=c.id
-            WHERE u.username || u.display_name LIKE "%${search}%" 
-            ORDER BY ${field} ${typeSort}
-            LIMIT ${limit} OFFSET ${offset}`, (err, result) => {
+        left join category as c on p.categoryID=c.id`, (err, result) => {
         if (err) {
           reject(err);
         } else {
