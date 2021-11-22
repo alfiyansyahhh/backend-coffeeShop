64ed494e4d1baa5114c6e439fc1007149adef703
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-11-22 11:41:27 +0700

    add

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 const { failed } = require('../helpers/respon');
+const { JWT_SECRET } = require('../helpers/env');
 
 const authentication = (req, res, next) => {
   const { token } = req.headers;
-  console.log(token);
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       failed(res, 401, err);
     } else {
